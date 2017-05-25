d73ca3b436b32a9757b4ff5da413aceced54d82c
Shinichi Shinoda <s-shinoda@sakura.ad.jp>
2017-05-25 14:54:39 +0900

    Release v0.1.3

---
+++ readme.md
@@ -0,0 +1,7 @@
+# sakura-links
+
+さくらインターネットの各種サービスコントロールパネルリンク集
+
+Chrome拡張機能: [さくらのコントロールパネルリンク](https://chrome.google.com/webstore/detail/%E3%81%95%E3%81%8F%E3%82%89%E3%81%AE%E3%82%B3%E3%83%B3%E3%83%88%E3%83%AD%E3%83%BC%E3%83%AB%E3%83%91%E3%83%8D%E3%83%AB%E3%83%AA%E3%83%B3%E3%82%AF/lpjekhokckhcokjklglljenadonhfcdb)
+
+![sample](image.png)
---
+++ script/popup.js
@@ -0,0 +1,121 @@
+var links = [
+    {
+        index: 1,
+        label: 'レンタルサーバ',
+        image: './assets/rensaba.svg',
+        href : 'https://secure.sakura.ad.jp/rscontrol/'
+    },
+    {
+        index: 2,
+        label: 'VPS',
+        image: './assets/vps.svg',
+        href : 'https://secure.sakura.ad.jp/vps/'
+    },
+    {
+        index: 3,
+        label: 'クラウド',
+        image: './assets/cloud.svg',
+        href : 'https://secure.sakura.ad.jp/cloud/iaas/'
+    },
+    {
+        index: 4,
+        label: '専用サーバ/高火力',
+        image: './assets/sensaba_koukaryoku.svg',
+        href : 'https://secure.sakura.ad.jp/server'
+    },
+    {
+        index: 5,
+        label: 'sakura.io',
+        image: './assets/sakuraio.svg',
+        href : 'https://secure.sakura.ad.jp/iot/console/#/'
+    },
+    {
+        index: 6,
+        label: 'ウェブアクセラレータ',
+        image: './assets/wa.svg',
+        href : 'https://secure.sakura.ad.jp/webaccel/'
+    },
+    {
+        index: 7,
+        label: '会員メニュー',
+        image: './assets/kaiin.svg',
+        href : 'https://secure.sakura.ad.jp/menu/top/index.php'
+    },
+    {
+        index: 8,
+        label: 'サポート',
+        image: './assets/support.svg',
+        href : 'https://help.sakura.ad.jp/'
+    }
+];
+
+function getTextWidth(text) {
+    var span = document.createElement('span');
+    span.innerHTML = text;
+    document.body.appendChild(span);
+
+    setTimeout(() => {
+        document.body.removeChild(span);
+    }, 0);
+
+    return span.offsetWidth || 0;
+}
+
+function main() {
+    var sakuraLinks = document.getElementsByClassName('sakura-links')[0];
+    Sortable.create(sakuraLinks, {
+        animation: 100,
+        onEnd: function (event) {
+            var list = [];
+            Array.from(sakuraLinks.children).forEach(function (child) {
+                if (child.dataset.index) {
+                    list.push(child.dataset.index);
+                }
+            });
+
+            window.localStorage.setItem('userlist', list);
+        }
+    });
+
+    var userList = window.localStorage.getItem('userlist');
+    if (userList) {
+        var l = userList.split(',').map(function (a) {
+            return parseInt(a, 10);
+        });
+        links.sort(function (a, b) {
+            return l.indexOf(a.index) > l.indexOf(b.index);
+        });
+    }
+
+    // <li class="item" data-index={{link.index}}>
+    //   <a class="item__link" href={{link.href}} target="_blank">
+    //     <img class="link__image" src={{link.image}}>
+    //     <span class="link__title">{{linklabel}}</span>
+    //   </a>
+    // </li>
+    links.forEach(function (link) {
+        var li = document.createElement('li');
+        li.classList.add('item');
+        li.dataset.index = link.index;
+
+        var needsCompress = getTextWidth(link.label) > 96;
+
+        li.addEventListener('mouseenter', function (event) {
+            event.target.classList.add('item--hover');
+        });
+        li.addEventListener('mouseleave', function (event) {
+            event.target.classList.remove('item--hover');
+        });
+
+        li.innerHTML = [
+            '<a class="item__link" href="' + link.href + '" target="_blank">',
+            '<img class="link__image" src="' + link.image + '">',
+            '<span class="link__title' + (needsCompress ? ' link__title--compress' : '') + '">' + link.label + '</span>',
+            '</a>'
+        ].join('');
+
+        sakuraLinks.appendChild(li);
+    });
+}
+
+main();
